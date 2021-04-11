fe366c693d84fd10661eaf24d831d53b1ba58996
haley-weinstein <haley.anne.weinstein@gmail.com>
2021-04-11 09:50:43 -0700

    echo

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #define CHIP_6713
 #define BUF_SIZE 5000
 #define CHUNK_SIZE 200
+#define BP_MAX_COEFS 120
+#define PI 3.1415926
+#define DELAYS 10
 
 //Add board support libraries
 #include <c6x.h>
@@ -28,84 +31,59 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-//add library haley snagged for the wah
-#include <wahwah.h>
 
-float *buffer;
+float buffer[BUF_SIZE];
 
-float right, right_out, ret;
+float right, right_out, ret, delay_line;
 int iright;
-
-
-
-
-int writeIndex = 0;
-float damping = .05;
-int minf = 500;
-int maxf = 3000;
-int wah_freq = 2000;
-int sampling_freq = 8000;
-float damp = .05;
-
-int i, read1, read2, read3;
-
+int i, buf_idx, read_idx;
+int delay;
 Uint32 sample_pair, output;
 
-int read_idx = 0;
+int sampling_frequency = 8000;
 
 void main()
 {
     DSK6713_init();
+    CSL_init();
     DSK6713_AIC23_Config config = DSK6713_AIC23_DEFAULTCONFIG;
     DSK6713_AIC23_CodecHandle hCodec;
-    float* buffer = calloc(BUF_SIZE, sizeof(float));
+
     hCodec = DSK6713_AIC23_openCodec(0, &config);               //open codec with default configuration
     DSK6713_AIC23_setFreq(hCodec, DSK6713_AIC23_FREQ_8KHZ);
     bool echo = true;
     bool wahwah = false;
+    double delay_time = .1;
+
     for(i=0; i<BUF_SIZE; i++){
         buffer[i] = 0;
     }
-    AutoWah_init(2000,  /*Effect rate 2000*/
-                 sampling_freq, /*Sampling Frequency*/
-                 1000,  /*Maximum frequency*/
-                 500,   /*Minimum frequency*/
-                 4,     /*Q*/
-                 0.707, /*Gain factor*/
-                 10     /*Frequency increment*/
-                 );
-    while(TRUE){
-        if (echo)
-        {
+
+    while(true){
+        if(echo){
             while(!DSK6713_AIC23_read(hCodec, &sample_pair));
             right =( (int) sample_pair) << 16 >> 16;
+            delay = sampling_frequency*delay_time;
 
-            read1 = (read_idx + 1000) % BUF_SIZE;
-            read2 = (read_idx + 2000) % BUF_SIZE;
-            read3 = (read_idx + 3000) % BUF_SIZE;
-            right_out = right+.9*buffer[read_idx] + .9*buffer[read1] +.9*buffer[read2]+.9*buffer[read3];
-            buffer[read_idx] = right_out; //try changing this to right if it sounds funky
+            for(i=0; i<DELAYS; i++)
+            {
+                buf_idx = (i+delay) % BUF_SIZE;
+                delay_line += (.9-i*.07)*buffer[buf_idx];
+            }
+
+            buffer[read_idx] = right;
             read_idx++;
             if(read_idx >= BUF_SIZE){
                 read_idx = 0;
             }
-
+            right_out = delay_line + right;
             iright = (int) right_out;
             output = (iright <<16)|(iright & 0x0000FFFF);
             while(!DSK6713_AIC23_write(hCodec, output));
-        }
-        if (wahwah){
-            while(!DSK6713_AIC23_read(hCodec, &sample_pair));
-            right =( (int) sample_pair) << 16 >> 16;
-            ret = AutoWah_process(right);
-            iright = (int) ret;
-            output = (iright <<16)|(iright & 0x0000FFFF);
-            while(!DSK6713_AIC23_write(hCodec, output));
-            AutoWah_sweep();
 
         }
-
-
     }
+
+
 }
 
